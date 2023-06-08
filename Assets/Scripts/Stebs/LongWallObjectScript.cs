5ea9d026f74fb0f0ecddab6c57da689e4b97a829
Steven Solof <stebssbets@yahoo.com>
2023-06-08 18:18:14 -0400

    import textmesh pro

--- Assets/Scripts/Stebs/LongWallObjectScript.cs
+++ Assets/Scripts/Stebs/LongWallObjectScript.cs
@@ -86,7 +86,7 @@ public class LongWallObjectScript : MonoBehaviour
         {
             if (highlighted)
             {
-                PushBackFullyBlockedEnemies();
+                //PushBackFullyBlockedEnemies();
 
                 MoveWallForward();
             }              
