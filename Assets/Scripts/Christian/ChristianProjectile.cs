c7c4e2f26dccd8cbf7411d1f1345e83a5c5e11cb
CTGabianelli11 <ctgabbianelli@gmail.com>
2023-06-23 18:23:55 -0400

    Added lifetime to projectiles

--- Assets/Scripts/Christian/ChristianProjectile.cs
+++ Assets/Scripts/Christian/ChristianProjectile.cs
@@ -7,10 +7,22 @@ public class ChristianProjectile : MonoBehaviour
     public float force;
     public Rigidbody rigidbody;
 
+    float timeAlive;
+    public float maxLifetime;
+
     public void FireProjectile(Vector3 position, Vector3 eulerAngles)
     {
         transform.position = position;
         transform.eulerAngles= eulerAngles;
         rigidbody.AddForce(force * transform.forward, ForceMode.Impulse);
+    }
+
+    private void Update()
+    {
+        if(timeAlive >= maxLifetime) {
+            Destroy(gameObject);
+        }
+
+        timeAlive += Time.deltaTime;
     }
 }
