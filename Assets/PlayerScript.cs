0723ce5ebb2d6835ef3c653c1b9a212cc9c0e5cc
bariskoklu <30669414+bariskoklu@users.noreply.github.com>
2023-07-27 00:27:40 +0300

    Grid system and baking navmesh when a wall is build, still WIP

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
@@ -11,9 +12,14 @@ public class PlayerScript : MonoBehaviour
 
     #region cached references
     [SerializeField] GameObject defaultPuzzlePiece;
+
+    MeshRenderer mouseIndicatorsMeshRenderer;
+
+    [SerializeField] Grid grid;
     #endregion
 
     private GameObject currentPuzzlePiece;
+    private Vector3 lastPositionForMouse;
 
     #region event subscriptions
     private void OnEnable()
@@ -34,6 +40,7 @@ public class PlayerScript : MonoBehaviour
     private void Start()
     {
         InstantiatePuzzlePiece();
+        NavigationBaker.Instance.BuildNavMesh();
     }
     private void Update()
     {
@@ -62,11 +69,30 @@ public class PlayerScript : MonoBehaviour
 
     private void InstantiatePuzzlePiece()
     {
-        // Get the mouse position in screen space
-        Vector3 mousePositionScreenSpace = Input.mousePosition;
+        Vector3 mousePositionWorldSpace;
+        if (GetSelectedMapPosition())
+        {
+            mousePositionWorldSpace = lastPositionForMouse;
+        }
+        else
+        {
+            // Get the mouse position in screen space
+            Vector3 mousePositionScreenSpace = Input.mousePosition;
 
-        // Convert the mouse position to world space
-        Vector3 mousePositionWorldSpace = Camera.main.ScreenToWorldPoint(new Vector3(mousePositionScreenSpace.x, mousePositionScreenSpace.y, 10f));
+            // Convert the mouse position to world space
+            mousePositionWorldSpace = Camera.main.ScreenToWorldPoint(new Vector3(mousePositionScreenSpace.x, mousePositionScreenSpace.y, 10f));
+
+            
+        }
+
+        // Apply clamping to the Y-coordinate
+        mousePositionWorldSpace.y = 0.5f;
+
+        //// Get the mouse position in screen space
+        //Vector3 mousePositionScreenSpace = Input.mousePosition;
+
+        //// Convert the mouse position to world space
+        //Vector3 mousePositionWorldSpace = Camera.main.ScreenToWorldPoint(new Vector3(mousePositionScreenSpace.x, mousePositionScreenSpace.y, 10f));
 
         // Instantiate the prefab at the mouse's position and make it the current puzzle piece
         currentPuzzlePiece = Instantiate(defaultPuzzlePiece, mousePositionWorldSpace, Quaternion.identity);
@@ -74,21 +100,73 @@ public class PlayerScript : MonoBehaviour
 
     private void MoveCurrentPuzzlePiece()
     {
-        // Get the mouse position in screen space
-        Vector3 mousePositionScreenSpace = Input.mousePosition;
+        Vector3 mousePositionWorldSpace;
+        if (GetSelectedMapPosition())
+        {
+            mousePositionWorldSpace = lastPositionForMouse;
+        }
+        else
+        {
+            // Get the mouse position in screen space
+            Vector3 mousePositionScreenSpace = Input.mousePosition;
 
-        // Convert the mouse position to world space
-        Vector3 mousePositionWorldSpace = Camera.main.ScreenToWorldPoint(new Vector3(mousePositionScreenSpace.x, mousePositionScreenSpace.y, 10f));
+            // Convert the mouse position to world space
+            mousePositionWorldSpace = Camera.main.ScreenToWorldPoint(new Vector3(mousePositionScreenSpace.x, mousePositionScreenSpace.y, 10f));
+
+            
+        }
 
         // Apply clamping to the Y-coordinate
         mousePositionWorldSpace.y = 0.5f;
+
 
         // Set the puzzle piece's position towards the mouse cursor smoothly
         currentPuzzlePiece.transform.position = Vector3.Lerp(currentPuzzlePiece.transform.position, mousePositionWorldSpace, Time.deltaTime * followSpeed);
     }
 
+    private bool GetSelectedMapPosition()
+    {
+        bool isGroundFound = false;
+
+        Vector3 mousePos = Input.mousePosition;
+        //Debug.Log("mousePos: " + mousePos);
+        mousePos.z = Camera.main.nearClipPlane;
+        //Debug.Log("mousePos.z: " + mousePos.z);
+
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+        RaycastHit raycastHit;
+
+        if (Physics.Raycast(ray, out raycastHit, 100/*, LayerMask.NameToLayer("Ground")*/))//
+        {
+            //if (raycastHit.transform.tag == "Ground")
+            //{
+            //    mouseIndicatorsMeshRenderer.material = usableIndicatorMaterial;
+            //}
+            //else
+            //{
+            //    mouseIndicatorsMeshRenderer.material = unusableIndicatorMaterial;
+            //}
+
+
+            if (raycastHit.transform.tag == "Ground")
+            {
+                Vector3Int gridPosition = grid.WorldToCell(raycastHit.point);
+                lastPositionForMouse = grid.CellToWorld(gridPosition);
+                isGroundFound = true;
+            }
+        }
+
+        return isGroundFound;
+    }
+
     private void PlaceCurrentPuzzlePiece()
     {
+        if (!GetSelectedMapPosition())
+        {
+            return;
+        }
+        NavigationBaker.Instance.surfaces.Add(currentPuzzlePiece.GetComponent<NavMeshSurface>());
+        NavigationBaker.Instance.BuildNavMesh();
         currentPuzzlePiece = null;
 
         if (GameManagerScript.currentGameState == GameManagerScript.GameState.preparationStage)
