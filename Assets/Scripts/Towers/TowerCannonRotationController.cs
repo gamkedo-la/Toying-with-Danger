277000be5d2883fd39f82e2215d37328d02904d5
CTGabianelli11 <108819775+CTGabbianelli11@users.noreply.github.com>
2024-01-24 19:20:19 -0500

    Created a script to rotate tower cannons based on the closest enemy returned from the detection script

---
+++ Assets/Scripts/Towers/TowerCannonRotationController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script should only handle aiming of the cannon at a target
+/// </summary>
+public class TowerCannonRotationController : MonoBehaviour
+{
+    public TowerDetectionScript towerDetectionScript;
+
+    public bool aimsAtTarget;
+
+    GameObject targetToAimAt;
+
+    //every frame the rotation should update to look at a target if specified
+    //too and there is a target to aim at
+    private void Update()
+    {
+        if(aimsAtTarget)
+            targetToAimAt = towerDetectionScript.GetClosestTarget();
+        else
+            targetToAimAt = null;
+        
+        if (targetToAimAt != null)        
+            transform.LookAt(targetToAimAt.transform);
+        
+    }
+
+    //logic for setting the towers target
+    public void SetAimTarget(GameObject targetToAimAt)
+    {
+        this.targetToAimAt = targetToAimAt;
+    }
+}
